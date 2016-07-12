72124e1385a7a5052ab3ae0546e8b2d4d9a415e0
rajputashu <ashurajput11@gmail.com>
2016-07-12 19:52:53 +0530

    ProjectAdd

---
+++ app/src/main/java/com/aad/ar/as/hawkeye/UserDetailInfo/UserDetailInformation.java
@@ -0,0 +1,18 @@
+package com.aad.ar.as.hawkeye.UserDetailInfo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aad.ar.as.hawkeye.R;
+
+/**
+ * Created by cirmac3 on 7/12/16.
+ */
+public class UserDetailInformation extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_detail_info);
+    }
+}
